89a77f62d301b1a110aeadc549c133664353efb5
Szymon Miedziejko <miedziejko@gmail.com>
2019-01-12 20:15:04 +0000

    Add Logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +9,7 @@ class App extends Component {
         <header className="App-header">
           Deck of Cards
         </header>
+        <img src={logo} className="App-logo" alt="logo" />
       </div>
     );
   }
