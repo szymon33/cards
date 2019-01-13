aae970debdf621f06a79b5e3d894bddda9898ea7
Szymon Miedziejko <miedziejko@gmail.com>
2019-01-13 23:20:44 +0000

    Improve CSS style

--- src/UserTable.js
+++ src/UserTable.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class UserTable extends Component {
   setupBgColor(val) {
-    return (val[0] == 'H' || val[0] == 'C') ? 'red' : 'black';
+    return (val[0] == 'H' || val[0] == 'D') ? 'red' : 'black';
   }
 
   renderList() {
@@ -10,15 +10,18 @@ class UserTable extends Component {
     if (cards != undefined && cards.length != 0) {
       return (
         cards.map((card, i) =>
-          <div key={i}
-               className={'pure-u-1-12 card  ' + this.setupBgColor(card) }
-               onClick={() => this.props.giveCardBack(card) }>
-            {card}
+          <div className='pure-u-1 pure-u-sm-1-2 pure-u-md-1-4 pure-u-lg-1-8'>
+            <div key={card}
+                 className={'card ' + this.setupBgColor(card) }
+                 onClick={() => this.props.giveCardBack(card) }>
+              <div>{card[0]}</div>
+              <div>{card.slice(1, 3)}</div>
+            </div>
           </div>
         )
       );
     } else {
-      return <div className='pure-u-1-1'>No card on the table. Take a card.</div>
+      return <div className='pure-u-1-1 no-cards-msg'>No card on the table. Take a card or shuffle.</div>
     }
   }
 
