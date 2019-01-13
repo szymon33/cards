ac3dc29101b699232921ff19e384f715516fc8c9
Szymon Miedziejko <miedziejko@gmail.com>
2019-01-13 15:05:53 +0000

    Add Game class. The logic of the app.

---
+++ src/Game.js
@@ -0,0 +1,73 @@
+class Game {
+  constructor() {
+    this.orderedDeck = this._buildDeck();
+    this.startOver();
+  }
+
+  takeOneCard() {
+    if (this.deck.length === 0) return null;
+
+    let card = this.deck.pop(1);
+    this.table.push(card);
+    return card;
+  }
+
+  giveCardBack() {
+    if (this.table.length === 0) return null;
+
+    let card = this.table.pop(1);
+    this.deck.push(card);
+    return card;
+  }
+
+  shuffle() {
+    if (this.deck.length === 0) { return null };
+
+    let ctr = this.deck.length, temp, randomIdx;
+
+    while (ctr > 0) {
+      randomIdx = Math.floor(Math.random() * ctr);
+
+      ctr--;
+
+      temp = this.deck[ctr];
+      this.deck[ctr] = this.deck[randomIdx];
+      this.deck[randomIdx] = temp;
+    }
+    return this.deck;
+  }
+
+  startOver() {
+    this.table = [];
+    this.deck = Array.from(this.orderedDeck);
+    return this;
+  }
+
+  sortTable() {
+    let tableDup = Array.from(this.orderedDeck);
+    if (this.table.length === 0) return this.table;
+
+    tableDup.sort( (a, b) => {  
+      return this.orderedDeck.indexOf(a) - this.orderedDeck.indexOf(b);
+    });
+    this.table = tableDup;
+    return this.table;
+  }
+
+  // private
+
+  _buildDeck() {
+    let result = [],
+        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'],
+        suits = ['C', 'S', 'H', 'D']; // clubs, spades, hearts, diamonds
+
+    ranks.forEach(function(rank) {
+      suits.forEach(function(suit){
+        result.push(rank.concat(suit));       
+      });
+    });
+    return result;
+  }
+}
+
+export default Game;
---
+++ src/Game.test.js
@@ -0,0 +1,133 @@
+import Game from './Game';
+
+describe('Game', () => {
+  let game = new Game();
+
+  beforeEach(() => {
+     game = new Game();
+  });
+
+  it('has table array', () => {
+    let table = game.table;
+    expect(table).toBeInstanceOf(Array);
+    expect(table).toHaveLength(0);
+  });
+
+  describe('_buildDeck', () => {
+    let deck = game._buildDeck();
+
+    it('is array', () => {
+      expect(deck).toBeInstanceOf(Array);
+    });
+
+    it('has length 52', () => {
+      expect(deck.length).toEqual(52);
+    });
+
+    it('has 2nd of club as first', () => {
+      expect(deck[0]).toBe('2C');
+    });
+
+    it('has Ace of dimond as last', () => {
+      expect(deck[51]).toBe('AD');
+    });
+  });
+
+  describe('takeOneCard', () => {
+    it('returns last card', () => {
+      expect(game.takeOneCard()).toEqual('AD');
+    });
+
+    it('decreases the deck', () => {
+      let deckInitialSize = game.deck.length;
+      game.takeOneCard();
+      expect(game.deck.length).toEqual(deckInitialSize - 1);
+    });
+
+    it('increases the table', () => {
+      let tableInitialSize = game.table.length;
+      game.takeOneCard();
+      expect(game.table.length).toEqual(tableInitialSize + 1);
+    });
+
+    it('the card on the table is not in the deck', () => {
+      let card = game.takeOneCard();
+      expect(game.table).toContain(card);
+      expect(game.deck).not.toContain(card);
+    });
+
+    it('returns null when deck is already empty', () => {
+      expect(game.takeOneCard).toBeNull;
+    });
+  });
+
+  describe('giveCardBack', () => {
+    it('returns last card', () => {
+      game.takeOneCard();
+      expect(game.giveCardBack()).toEqual('AD');
+    });
+
+    it('increases the deck', () => {
+      game.takeOneCard();
+      let deckInitialSize = game.deck.length;
+      game.giveCardBack();
+      expect(game.deck.length).toEqual(deckInitialSize + 1);
+    });
+
+    it('decreases the table', () => {
+      game.takeOneCard();
+      let tableInitialSize = game.table.length;
+      game.giveCardBack();
+      expect(game.table.length).toEqual(tableInitialSize - 1);
+    });
+
+    it('the card is no longer in the table', () => {
+      let card = game.takeOneCard();
+
+      expect(game.table).toContain(card);
+      expect(game.deck).not.toContain(card);
+      game.giveCardBack();
+      expect(game.deck).toContain(card);
+      expect(game.table).not.toContain(card);
+    });
+
+    it('returns null when deck is already empty', () => {
+      expect(game.takeOneCard).toBeNull;
+    });
+  });
+
+  describe('shuffle', () => {
+    it('returns different deck', () => {
+      expect(game.deck).toEqual(game.orderedDeck);
+      game.shuffle();
+      expect(game.deck).not.toEqual(game.orderedDeck);
+    });
+  });
+
+  describe('startOver', () => {
+    it('clears the table', () => {
+      game.takeOneCard();
+      expect(game.table).not.toEqual([]);
+      game.startOver();
+      expect(game.table).toEqual([]);
+    });
+
+    it('gets back sorted deck', () => {
+      expect(game.deck).toEqual(game.orderedDeck);
+      game.shuffle();
+      expect(game.deck).not.toEqual(game.orderedDeck);
+    });
+  });
+
+  it('sortTable', () => {
+    game.shuffle();
+    expect(game.table).toEqual([]);
+    [1, 2 ,3].forEach(() => { game.takeOneCard(); });
+    let table = game.table;
+    expect(table).not.toEqual([]);
+    expect(table.length).toEqual(3);
+    game.sortTable();
+    expect(table.length).toEqual(3);
+    expect(table).not.toEqual(game.table);
+  });
+});
